58c9af04ae6d39d868d0d80552f1dcb6d28ac918
belabdifatimazohra <fatima.zohra.beabdi@gmail.com>
2020-11-11 21:44:20 +0100

    Add  Navbar component

---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Layout() {
+  return (
+    <div>
+      <h1>React Bootstrap Checkpoint</h1>
+      <img src="./images/bg1.jpg" alt="quote" />
+    </div>
+  );
+}
+
+export default Layout;
